290d61d24b3157c53e08b55f0bee23ad1937b08f
.\vite.config. <joaogammer10@gmail.com>
2025-03-31 08:30:47 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,42 @@ import { useState } from "react"
 
 function App(){
 
-  const [nome, setNome] = useState('Ronaldo')
+  
 
-  return (
-    <div>
-      <h3> Pizzaria 2B</h3>
-<input className="nome" 
+  const Formulario = (props) => {
+
+    const [nome, setNome] = useState('Ronaldo')
+
+   return (  
+   <div> 
+    <h3>{props.titulo}</h3>
+    <input className="nome" 
   placeholder="Digite seu nome..."
   onChange={(e)=>{setNome(e.target.value)}}
   name="nome"
   type="text" />
-<button className="botao" onClick={()=>alert(nome)}>
+<button className="botao" onClick={
+  ()=> {
+    if(nome.leght > 0)
+      alert (nome)
+    else
+    alert(props.mensagem)}
+  }>
+
     CLIQUE AQUI
 </button>  
+   </div>
+   )
+  }
+
+  return (
+    <div>
+      <h3> Pizzaria 2B</h3>
+<Formulario  titulo = "Nome" menssagem = "sardinmha"/>
+<Formulario titulo = "E-mail" mensagem = "curintia" />
+<Formulario titulo = "CPF" mensagem = "porcada" />
+<Formulario />
+
     </div>
   )
 }
